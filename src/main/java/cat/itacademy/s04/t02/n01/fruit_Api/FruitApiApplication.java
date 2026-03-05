84186a6d555816bcf92daa339007b8d0475f73bb
Javier Castillo <javiercame98@gmail.com>
2026-03-05 21:54:14 +0100

    initial commit and creating Fruit model

---
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit_Api/FruitApiApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t02.n01.fruit_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitApiApplication.class, args);
+	}
+
+}
